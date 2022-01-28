ac466e8c7a7581e0edfbefc04cd1b32d787dc812
jimmyhu997 <jimmywx@outlook.it>
2022-01-28 15:39:46 +0100

    end

---
+++ classes/movie.php
@@ -0,0 +1,86 @@
+<?php 
+
+class Movie{
+    private string $title;
+    private int $duration;
+    private int $release_year;
+    private float $budget;
+    private array $genre;
+    private string $language;
+    private string $country;
+
+    public function __construct($_title,$_release_year,$_language,$_country)
+    {
+        $this->setTitle($_title);
+        $this->setRelease_year($_release_year);
+        $this->setLanguage($_language);
+        $this->setCountry($_country);
+    }
+
+    public function setGenres($_genre){
+        $this->genre = $_genre;
+    }
+    
+    public function setTitle($_title){
+        $this->title = $_title;
+    }
+    
+    public function setDuration($_duration){
+        $this->duration = $_duration;
+    }
+    
+    public function setRelease_year($_release_year){
+        $this->release_year = $_release_year;
+    }
+    
+    public function setBudget($_budget){
+        $this->budget = $_budget;
+    }
+    
+    public function setLanguage($_language){
+        $this->language = $_language;
+    }
+    
+    public function setCountry($_country){
+        $this->country = $_country;
+    }
+    
+
+    public function getGenres(){
+        return $this->genre;
+    }
+
+    public function getTitle(){
+        return $this->title;
+    }
+
+    public function getDuration(){
+        return $this->duration;
+    }
+
+    public function getRelease_year(){
+        return $this->release_year;
+    }
+
+    public function getBudget(){
+        return $this->budget;
+    }
+
+    public function getLanguage(){
+        return $this->language;
+    }
+
+    public function getCountry(){
+        return $this->country;
+    }
+
+    public function getNameYear(){
+        return "{$this->title} {$this->release_year}";
+    }
+}
+
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,47 @@
+<?php 
+require_once __DIR__ . '/classes/movie.php';
+
+$movies = [
+    $forest_gump = new Movie('Forest Gump', 1994 , 'English','USA'),
+    $The_Godfather = new Movie('The Godfather', 1972 , 'English','USA'),
+    
+];  
+// forest gump data declaration
+$forest_gump->setGenres(['Comedy','Drama']);
+$forest_gump->setDuration(142);
+$forest_gump->setBudget(55000000.00);
+
+//The_Godfather data declaration
+$The_Godfather->setGenres(['Noir','Drama','Gangster']);
+$The_Godfather->setDuration(175);
+$The_Godfather->setBudget(6000000.00);
+
+
+// var_dump($forest_gump);
+// echo "<br>";
+// echo "<br>";
+// echo "<br>";
+// var_dump($The_Godfather);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php foreach($movies as $movie){?>
+        <h2><?php echo $movie->getNameYear(); ?></h2>
+        <ul>
+            <li>Duration: <?php echo "{$movie->getNameYear()}"; ?></li>
+            <li>Budget: <?php echo "{$movie->getBudget()}"; ?></li>
+            <li>Original Language: <?php echo "{$movie->getLanguage()}"; ?></li>
+            <li>Country: <?php echo "{$movie->getCountry()}"; ?></li>
+        </ul>
+    <?php } ?>
+</body>
+</html>
